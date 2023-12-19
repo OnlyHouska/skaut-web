4eabd0c89bf42541e80f07d6dcab775d8748e288
OnlyHouska <bun.noob.bun@gmail.com>
2023-12-20 00:57:56 +0100

    Finished gallery.html. Added info subpages to druziny.html, added historie.html

--- src/script/gallery.js
+++ src/script/gallery.js
@@ -1,5 +1,5 @@
 function openGallery(id) {
-  let driveLinks = ["https://google.com", "https://youtube.com", "https://discord.com"];
+  let driveLinks = ["https://drive.google.com/drive/folders/1Q8JeZ4HapgxeZB1oH7b8TdklDbFTjDtN?usp=drive_link", "https://drive.google.com/drive/folders/11Vtv0s2rGTOSDXwmruV2ioKdKMXoF1Xb?usp=drive_link", "https://drive.google.com/drive/folders/10ECPdXOpcRPAPmroTWrLQewr7vqJ6SLy?usp=drive_link"];
   page = driveLinks[id];
   window.open(page);
 }
